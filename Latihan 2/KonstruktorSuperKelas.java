49650b3e421edbfd825f8a65e7483a6b2d411687
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-05-31 11:31:24 +0700

    Create KonstruktorSuperKelas.java

---
+++ Latihan 2/KonstruktorSuperKelas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class KonstruktorSuperKelas {
+    public static void main (String[]args)
+    {
+        Employ card1 = new Employ ("123456778", "Yanto", 32);
+        card1.info ();
+    }
+}
