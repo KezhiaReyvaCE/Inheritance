bd285485346a3eeb3ddd96d1664f4212501e7c33
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-05-31 12:12:14 +0700

    Create KartuPerpus.java

---
+++ Tugas Praktikum 1/KartuPerpus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class KartuPerpus {
+    public static void main (String[]args)
+    {
+        ID card1 = new ID ("402151", "Kezhia Reyva C.E", 16);
+        card1.info ();
+    }
+}
